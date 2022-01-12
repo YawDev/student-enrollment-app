ccf0f4a1b2bb76d09b376d422de0ff6b0e28a085
YawDev <94807175+YawDev@users.noreply.github.com>
2022-01-11 20:35:41 -0500

    search

---
+++ StudentEnrollment.App/Models/SearchModel.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentEnrollment.App.Models
+{
+    public class SearchModel
+    {
+        public string Department { get; set; }
+        public string Section { get; set; }
+        public string Instructor { get; set; }
+        public string CourseName { get; set; }
+        public string Abbreviation { get; set; }
+        
+        
+        
+        
+        
+
+    }
+}
